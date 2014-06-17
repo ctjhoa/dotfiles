414ac708eb080295dc4bc14e2f9f58c1b51770e2
Camille TJHOA <camille.tjhoa@gmail.com>
2014-06-17 02:02:14 +0200

    Fix path issue | move tmux config to config folder

--- install.sh
+++ install.sh
@@ -9,12 +9,12 @@ confdir=`pwd`
 
 # Create directory links
 ln -sfT $confdir/emacs.d ~/.emacs.d
-ln -sfT $confdir/tmux ~/.tmux
+ln -sfT $confdir/config/tmux ~/.config/tmux
 ln -sfT $confdir/config/colors ~/.config/colors
 
 # Create file links
 ln -sf $confdir/compton.conf ~/.compton.conf
-ln -sf ~/.tmux/conf ~/.tmux.conf
+ln -sf ~/tmux/conf ~/.tmux.conf
 ln -sf $confdir/xinitrc ~/.xinitrc
 ln -sf $confdir/Xresources ~/.Xresources
 ln -sf $confdir/zprofile ~/.zprofile
