89501122af9fb8b7efda8dff792d2ff091cabf00
Camille TJHOA <camille.tjhoa@gmail.com>
2014-11-04 00:44:41 +0100

    Add several packages

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -27,7 +27,6 @@
 (load custom-file)
 
 (toggle-truncate-lines t)
-(fset 'yes-or-no-p 'y-or-n-p)
 
 ; Auto install use-package
 (if (not (package-installed-p 'use-package))
@@ -56,7 +55,8 @@
 (use-package smart-mode-line
   :ensure
   :pre-load (setq sml/no-confirm-load-theme t)
-  :init (sml/setup)
+  :init
+  (sml/setup)
   :config
   (sml/apply-theme 'respectful)
   (set-face-attribute 'mode-line nil
@@ -67,12 +67,6 @@
 		      :foreground "gray15"
 		      :background "black"
 		      :box '(:color "black" :line-width 6)))
-
-
-(use-package volatile-highlights
-  :ensure
-  :diminish volatile-highlights-mode
-  :init (volatile-highlights-mode t))
 
 (use-package highlight-numbers
   :ensure
@@ -96,23 +90,6 @@
   (progn
     (define-key ido-file-completion-map  (kbd "  ") 'ido-prev-match)
     (define-key ido-file-completion-map  (kbd "<tab>") 'ido-next-match))
-;  (use-package ido-other-window
-;    :ensure
-;    :config
-;    (defun my-ido-split-keys ()
-;      "Add my keybindings to enable window splitting with ido."
-;      (mapcar
-;       (lambda (map)
-;	 (define-key map (kbd "<C-return>") 'ido-exit-minibuffer)
-;	 (define-key map (kbd "C-o") 'ido-invoke-in-other-window)
-;	 (define-key map (kbd "C-h") 'ido-invoke-in-vertical-split)
-;	 (define-key map (kbd "C-v") 'ido-invoke-in-horizontal-split)
-;	 (define-key map (kbd "C-t") 'ido-invoke-in-new-frame))
-;       (list ido-buffer-completion-map
-;	     ido-common-completion-map
-;	     ido-file-completion-map
-;	     ido-file-dir-completion-map)))
-;  (add-hook 'ido-setup-hook 'my-ido-split-keys))
   (use-package ido-vertical-mode
     :ensure
     :init (ido-vertical-mode t))
@@ -132,11 +109,22 @@
     :bind ("M-x" . smex)))
 
 
+(use-package yasnippet
+  :ensure
+  :diminish yas-minor-mode
+  :commands yas-global-mode
+  :idle (yas-global-mode)
+  :init
+  (setq yas-verbosity 1
+	yas-prompt-functions '(yas-completing-prompt yas-ido-prompt)))
+
 (use-package company
   :ensure
-  :idle (global-company-mode t)
+  :commands global-company-mode
+  :idle (global-company-mode)
   :config
   (progn
+    (add-to-list 'company-backends 'company-yasnippet)
     (define-key company-mode-map (kbd "C-n") 'company-select-next)
     (define-key company-mode-map (kbd "C-p") 'company-select-previous)
     (define-key company-active-map (kbd "C-d") 'company-show-doc-buffer)
@@ -150,33 +138,100 @@
   :config
   (progn 
     (evil-mode t)
-    (define-key evil-ex-map (kbd "e ") 'ido-find-file)
-    (define-key evil-ex-map (kbd "b ") 'ido-switch-buffer)))
+    (evil-add-hjkl-bindings magit-status-mode-map 'emacs
+      ":" 'evil-ex
+      "K" 'magit-discard-item
+      "l" 'magit-key-mode-popup-logging
+      "h" 'magit-diff-toggle-refine-hunk)
+    ;; move RET and SPC from motion state to normal state
+    (mapcar
+     (lambda (k)
+       (define-key evil-normal-state-map k (lookup-key evil-motion-state-map k))
+       (define-key evil-motion-state-map k nil))
+     (list (kbd "RET") " "))
+    ;; ex-mode shortcuts
+    (define-key evil-ex-map "e " 'ido-find-file)
+    (define-key evil-ex-map "b " 'ido-switch-buffer)
+    (define-key evil-ex-map "pf " 'projectile-find-file)
+    (define-key evil-ex-map "pp " 'projectile-switch-project)
+    (define-key evil-ex-map "pa " 'projectile-ag)
+    (define-key evil-ex-map "pb " 'projectile-switch-to-buffer)
+    (define-key evil-ex-map "po " 'projectile-find-other-file)
+    ;; Vim-ism ;]
+    (define-key evil-normal-state-map "Y" (kbd "y$"))
+    ;; ESC exit from anywhere
+    (define-key evil-normal-state-map [escape] 'keyboard-quit)
+    (define-key evil-visual-state-map [escape] 'keyboard-quit)
+    (define-key minibuffer-local-map [escape] 'abort-recursive-edit)
+    (define-key minibuffer-local-ns-map [escape] 'abort-recursive-edit)
+    (define-key minibuffer-local-completion-map [escape] 'abort-recursive-edit)
+    (define-key minibuffer-local-must-match-map [escape] 'abort-recursive-edit)
+    (define-key minibuffer-local-isearch-map [escape] 'abort-recursive-edit))
+  (use-package evil-visualstar
+    :ensure)
+  (use-package evil-jumper
+    :ensure)
+  (use-package evil-matchit
+    :ensure)
+  (use-package evil-surround
+    :ensure
+    :commands global-evil-surround-mode
+    :idle (global-evil-surround-mode)
+    :config
+    (setq-default surround-pairs-alist
+		  '((?\( . ("(" . ")"))
+		    (?\[ . ("[" . "]"))
+		    (?\{ . ("{" . "}"))
+		    (?\) . ("( " . " )"))
+		    (?\] . ("[ " . " ]"))
+		    (?\} . ("{ " . " }"))
+		    (?# . ("#{" . "}"))
+		    (?b . ("(" . ")"))
+		    (?B . ("{" . "}"))
+		    (?> . ("<" . ">"))
+		    (?t . evil-surround-read-tag)
+		    (?< . evil-surround-read-tag)
+		    (?f . evil-surround-function)))))
 
 ;(use-package vi-tilde-fringe-mode
 ;  :ensure)
 
 ;; Per languages
 ;(add-hook 'prog-mode-hook 'vi-tilde-fringe-mode)
-(add-hook 'emacs-lisp-mode-hook
-	  (lambda()
-	    (use-package eldoc
-	      :ensure
-	      :diminish eldoc-mode
-	      :init
-	      (progn 
-		(eldoc-mode t)))
-	    (use-package highlight-parentheses
-	      :ensure
-	      :init
-	      (progn 
-		(highlight-parentheses-mode t)))
-	    (use-package rainbow-delimiters
-	      :ensure
-	      :init
-	      (progn 
-		  (rainbow-delimiters-mode t)))
-	    (electric-pair-mode t)))
+
+(use-package highlight-parentheses
+  :init
+  (progn 
+    (add-hook 'emacs-lisp-mode-hook (lambda()
+				      (eldoc-mode t)))))
+(use-package highlight-parentheses
+  :init
+  (progn 
+    (add-hook 'emacs-lisp-mode-hook (lambda()
+				      (electric-pair-mode t)))))
+(use-package highlight-parentheses
+  :ensure
+  :init
+  (progn 
+    (add-hook 'emacs-lisp-mode-hook (lambda()
+				      (highlight-parentheses-mode t)))))
+(use-package rainbow-delimiters
+  :ensure
+  :init
+  (progn 
+    (add-hook 'emacs-lisp-mode-hook (lambda()
+				      (rainbow-delimiters-mode t)))))
+
+(use-package rainbow-mode
+  :ensure
+  :init
+  (progn 
+    (add-hook 'emacs-lisp-mode-hook (lambda()
+				      (rainbow-mode t)))
+    (add-hook 'css-mode-hook (lambda()
+				      (rainbow-mode t)))
+    (add-hook 'html-mode-hook (lambda()
+				      (rainbow-mode t)))))
 
 (use-package js2-mode
   :ensure
